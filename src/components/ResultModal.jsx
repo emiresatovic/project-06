41bc338d0a7ef8a38916dd584131848a4bd9d3b0
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2024-10-08 10:38:20 +0200

    08.10 10h

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,17 @@
+
+import { forwardRef } from 'react';
+
+const ResultModal =  forwardRef (function ResultModal({ result, targetTime}, ref) {
+    return(
+        <dialog ref={ref} className="result-modal">
+            <h2>You {result}</h2>
+            <p>THe target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>x</strong> seconds left.</p>
+            <form method='dialog'>
+                <button>Close</button>
+            </form>
+        </dialog>
+    );
+})
+
+export default ResultModal;
